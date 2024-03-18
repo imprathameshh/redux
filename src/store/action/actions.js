65408fc640051b01bfbbf089b49a3b3d6ad9b428
prathmesh_kwt <prathmesh@krishaweb.com>
2024-03-18 15:44:33 +0530

    final-commit

--- src/store/action/actions.js
+++ src/store/action/actions.js
@@ -1,7 +1,7 @@
+//FIRST STEP
 export const SET_FORM_VALUE = "SET_FORM_VALUE";
 
 export const setFormDetails = (params) => {
-  console.log(params);
   return {
     type: SET_FORM_VALUE,
     // UPDATE A VALUE
--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -5,7 +5,6 @@ const initialState = {
 };
 
 const userReducer = (state = initialState, action) => {
-  console.log(state);
   if (action.type === SET_FORM_VALUE) {
     return {
       ...state, // FOR OLD VALUE
